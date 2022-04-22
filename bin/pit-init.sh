2f4fb8465c33b2f5b5c09d335149acbe19d3ffcb
Russell Bunch <doomslayer@hpe.com>
2022-04-22 14:44:01 -0500

    CASMINST-4485 Handle YQ Merge (#27)
    A subset of the generated data from `csi config init` needs to be merged
    into the site-init/customizations.yaml file. PIT Init calls `csi config
    init` before copying the generated files into place, but it fails to
    handle the merge into customizations.yaml. This changes fixes that gap
    by invoking yq to merge that newly generated data into
    customizatons.yaml. PIT Init will do this before patching the CA
    certificate. All that needs to happen now from a pit-init perspective is
    the CSI files must be in place, and site-init must be created.
    
    The docs will still entail calling `csi config init`, and then creating
    site-init before calling pit-init. However this change will ensure that
    any call to pit-init ensures that customizations.yaml has any updated
    delta.

--- bin/pit-init.sh
+++ bin/pit-init.sh
@@ -23,7 +23,9 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 
 # Author: rustydb <doomslayer@hpe.com>
-PITDATA=/var/www/ephemeral
+set -e
+ERROR=0
+
 echo 'Initializing the Pre-Install Toolkit'
 echo 'Exporting tokens'
 set -x
@@ -39,11 +41,17 @@ function die () {
     exit 1
 }
 
+function error() {
+    echo >&2 "ERROR: "${1}""
+    ERROR=1
+}
+
 export username=${username:-$(whoami)}
 bmc_password=${IPMI_PASSWORD:-''}
 [ -z "$bmc_password" ] && die 'Need IPMI_PASSWORD exported to the environment (optionally, "export username=myuser" as well otherwise $(whoami) is assumed.)'
 
 if [ -z "$SYSTEM_NAME" ] ; then
+    # There is no LAN at this point, so no rDNS can be done to resolve this.
     echo 'SYSTEM_NAME was not set; resolving automatically ... '
     # foo-ncn-m001-pit  ---> foo
     # foo-ncn-m001      ---> foo
@@ -53,51 +61,91 @@ if [ -z "$SYSTEM_NAME" ] ; then
 fi
 
 if [ -e /dev/disk/by-label/PITDATA ]; then
+    set +e
     mount -v -L PITDATA
+    set -e
 fi
 
-PREP_DIR=/var/www/ephemeral/prep
-CSI_CONF=$PREP_DIR/system_config.yaml
-set -e
-if [ ! -d $PREP_DIR ] ; then
-    echo >&2 "Need $PREP_DIR; this needs to be created by hand if this is a first-time install."
-    echo >&2 "See: https://github.com/Cray-HPE/docs-csm/blob/main/install/bootstrap_livecd_usb.md#configuration-payload"
-    exit 1
-elif [ ! -f $CSI_CONF ] ; then
-    echo >&2 'CSI needs inputs; no $CSI_CONF file detected!'
-    echo >&2 'See: https://github.com/Cray-HPE/docs-csm/blob/main/install/bootstrap_livecd_usb.md#generate-installation-files'
-    exit 1
+# Create our base directories if they do not already exist.
+PIT_DATA=/var/www/ephemeral
+PREP_DIR=${PIT_DATA}/prep
+DATA_DIR=${PIT_DATA}/data
+CONF_DIR=${PIT_DATA}/configs
+CSI_CONF=${PREP_DIR}/system_config.yaml
+SITE_INIT=${PREP_DIR}/site-init
+[ ! -d $PREP_DIR ] && mkdir -pv $PREP_DIR
+[ ! -d $DATA_DIR ] && mkdir -pv $DATA_DIR
+[ ! -d $CONF_DIR ] && mkdir -pv $CONF_DIR
+if [ ! -f $CSI_CONF ] ; then
+    # TODO: MTL-1695 Update this to point to the new example system_config.yaml file.
+    error 'CSI needs inputs; no $CSI_CONF file detected!'
+    die 'See: https://github.com/Cray-HPE/docs-csm/blob/main/install/prepare_configuration_payload.md'
 fi
+
+# Resolve SITE_INIT.
+site_init_error=0
+if [ ! -d $SITE_INIT ] ; then
+    error "Need $SITE_INIT; this needs to be created before invoking $0!"
+    error "See: https://github.com/Cray-HPE/docs-csm/blob/main/install/prepare_site_init.md#create-and-initialize-site-init-directory"
+    site_init_error=1
+fi
+
+# Resolve CSM_PATH and the yq binary.
+# TODO: MTL-1682 install hpe-csm-yq-package
+yq_error=0
+YQ_BINARY=/usr/bin/yq
+if [ -z ${CSM_PATH} ] ; then
+    error "Can not find CSM tarball providing the yq binary, CSM_PATH is empty!"
+    yq_error=1
+elif [ ! -d ${CSM_PATH} ] ; then
+    error "Can not find CSM_PATH: $CSM_PATH ; no such directory"
+    yq_error=1
+elif [ ! -f ${YQ_BINARY} ] ; then
+    YQ_BINARY="${CSM_PATH}/shasta-cfg/utils/bin/$(uname | awk '{print tolower($0)}')/yq"
+    if [ ! -f ${YQ_BINARY} ] ; then
+        error "Can not find yq binary at $YQ_BINARY"
+        yq_error=1
+    fi
+fi
+
 echo 'Generating Configuration ...'
 (
-    pushd /var/www/ephemeral/prep
-    rm -rf $SYSTEM_NAME && csi config init
+    pushd $PREP_DIR
+    [ -d $SYSTEM_NAME ] && mv $SYSTEM_NAME $SYSTEM_NAME-$(date '+%Y%m%d%H%M%S')
+    csi config init
     cp -pv $SYSTEM_NAME/pit-files/* /etc/sysconfig/network/
     cp -pv $SYSTEM_NAME/dnsmasq.d/* /etc/dnsmasq.d/
     cp -pv $SYSTEM_NAME/basecamp/data.json /var/www/ephemeral/configs/
     cp -pv $SYSTEM_NAME/conman.conf /etc
     popd
 )
-
-SHASTA_CFG_DIR=$PREP_DIR/site-init
-if [ ! -d $SHASTA_CFG_DIR ] ; then
-    echo >&2 "Need $SHASTA_CFG_DIR; this needs to be created by hand if this is a first-time install."
-    echo >&2 "See: https://github.com/Cray-HPE/docs-csm/blob/main/install/prepare_site_init.md#create-and-initialize-site-init-directory"
-    exit 1
+if [ $yq_error = 0 ] ; then
+    echo 'Merging Generated IPs into customizations.yaml'
+    "$YQ_BINARY" merge -xP -i ${SITE_INIT}/customizations.yaml <($YQ_BINARY prefix -P "${PREP_DIR}/${SYSTEM_NAME}/customizations.yaml" spec)
+else
+    error 'yq is not available for merging generated IPs into customizations.yaml!'
+fi 
+if [ $site_init_error = 0 ] ; then
+    echo 'Patching CA into data.json (cloud-init)'
+    csi patch ca --cloud-init-seed-file ${CONF_DIR}/data.json --customizations-file ${SITE_INIT}/customizations.yaml --sealed-secret-key-file ${SITE_INIT}/certs/sealed_secrets.key
+else
+    error "site-init does not exist at the expected location: $SITE_INIT"
 fi
-echo 'Patching CA into data.json (cloud-init)'
-csi patch ca --cloud-init-seed-file $PITDATA/configs/data.json --customizations-file $PITDATA/prep/site-init/customizations.yaml --sealed-secret-key-file $PITDATA/prep/site-init/certs/sealed_secrets.key
 
 echo 'Loading sysconfig (ifcfg files) ... '
-echo >&2 'WARNING: SSH may disconnect if it was already setup'
-
+echo 'WARNING: SSH may disconnect if it was already setup'
 set +e
 wicked ifreload all
 set -e
 
-systemctl enable conman
-
+# nexus takes longer to start, this ensures we fail-quickly on basecamp, conman, or dnsmasq if nexus is started by itself.
+systemctl enable basecamp conman dnsmasq nexus
 echo 'Restarting basecamp conman dnsmasq ... ' && systemctl restart basecamp conman dnsmasq
 echo 'Restarting nexus ... ' && systemctl restart nexus
 
-echo 'Pre-Install Toolkit has been initialized ... '
+if [ $ERROR = 0 ]; then
+    echo 'Pre-Install Toolkit has been initialized ...'
+else
+    echo >&2 'Pre-Install Toolkit failed to initialize ...'
+    echo >&2 'Please inspect the above output for any and all errors before moving on with a CSM install.'
+fi
