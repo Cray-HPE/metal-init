c08bae43f5e373c5b653505ccc5db2031fc19c64
rustydb <doomslayer@hpe.com>
2022-05-07 21:36:45 -0500

    Download both image types at the same time

--- bin/get-sqfs.sh
+++ bin/get-sqfs.sh
@@ -22,37 +22,27 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
-# usage: get-sqfs.sh -k 0.0.8
-# usage: get-sqfs.sh -s 0.0.7
-# usage: get-sqfs.sh -k f1e225a-1609179792335
+# usage: get-sqfs.sh 0.0.8
+# usage: get-sqfs.sh 0.0.7
+# usage: get-sqfs.sh f1e225a-1609179792335
 # note: pairs great with set-sqfs-links.sh
 
-t=$1
-id=$2
-[ -z $2 ] && id=$1
-[ -z $id ] && exit 1
-
-case "${t}" in
-    -s)
-        dir='ceph'
-        type='storage-ceph'
-        ;;
-    -k)
-        dir='k8s'
-        type='kubernetes'
-        ;;
-    *)
-        dir='k8s'
-        type='kubernetes'
-        ;;
-esac
+id=$1
+if [ -z $id ]; then
+    echo >&2 'Missing image ID (e.g. X.Y.Z or COMMIT-TIMESTAMP'
+    exit 1
+fi
 
 stream=unstable
 if [[ "$id" =~ [0-9]*\.[0-9]*\.[0-9]*$ ]]; then
     stream=stable
 fi
 
-mkdir -pv /var/www/ephemeral/data/${dir}
-pushd /var/www/ephemeral/data/${dir}
-wget --mirror -np -nH --cut-dirs=4 -A *.kernel,*initrd*,*.squashfs -nv https://artifactory.algol60.net/artifactory/csm-images/${stream}/${type}/${id}/
+mkdir -pv /var/www/ephemeral/data/ceph
+pushd /var/www/ephemeral/data/ceph
+wget --mirror -np -nH --cut-dirs=4 -A *.kernel,*initrd*,*.squashfs -nv https://artifactory.algol60.net/artifactory/csm-images/${stream}/storage-ceph/${id}/
+popd
+mkdir -pv /var/www/ephemeral/data/k8s
+pushd /var/www/ephemeral/data/k8s
+wget --mirror -np -nH --cut-dirs=4 -A *.kernel,*initrd*,*.squashfs -nv https://artifactory.algol60.net/artifactory/csm-images/${stream}/kubernetes/${id}/
 popd
